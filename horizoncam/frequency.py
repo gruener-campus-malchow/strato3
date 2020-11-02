fcc2b8bb3d2522ea8e0a6f2a944ba15c33f409cf
Your Name <you@example.com>
2020-11-02 12:42:50 +0100

    added calculation for frequency

--- horizoncam/frequency.py
+++ horizoncam/frequency.py
@@ -5,22 +5,30 @@ import _thread as thread
 import time
 
 conf = configparser.ConfigParser()
-conf.read('/home/video/strato3/horizoncam/mission_conf.ini')
+conf.read('/home/begafoe/Schreibtisch/strato3/horizoncam/mission_conf.ini')
+onlineThreads = []
 
-def printFrequency(Phase, frequency):
-    print(Phase + " " + frequency)
 
-   # time.sleep(1)
-
+def PhotoThread(Phase, frequency, endTime):
+    #print(Phase + " " + frequency)
+    now = datetime.datetime.now()
+    sleepTime = round(60 / frequency)
+    while (now < endTime):
+        print(now)
+        print(sleepTime)
+        time.sleep(sleepTime)
+        now = datetime.datetime.now()
+    
 while 1:
     now = datetime.datetime.now()
     for section in conf.sections():
         begin = datetime.datetime.strptime(conf.get(section, 'begin'), '%Y-%m-%d %H:%M')
         end = datetime.datetime.strptime(conf.get(section, 'end'), '%Y-%m-%d %H:%M')
-        if (now >= begin and now < end):
-            PhotosPerMinute = conf.get(section, 'PhotosPerMinute')
+        if (now >= begin and now < end and not section in onlineThreads):
+            PhotosPerMinute = int(conf.get(section, 'PhotosPerMinute'))
             try:
-                thread.start_new_thread( printFrequency, (section, PhotosPerMinute,  ) )
+                thread.start_new_thread( PhotoThread, (section, PhotosPerMinute, end, ) )
+                onlineThreads.append(section)
             except:
                 print ("Error: unable to start thread")
     time.sleep(60)
