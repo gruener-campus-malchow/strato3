bacc2503bbfee85dced6399c28678400f3c9682c
TenkouNoKitsune <66792136+TenkouNoKitsune@users.noreply.github.com>
2021-09-17 18:13:39 +0200

    Updated frequency.py and mission_conf.ini

--- horizoncam/frequency.py
+++ horizoncam/frequency.py
@@ -9,30 +9,31 @@ conf = configparser.ConfigParser()
 conf.read('/home/pi/strato3/horizoncam/mission_conf.ini')
 onlineThreads = []
 
-
 def PhotoThread(Phase, frequency, endTime):
     #print(Phase + " " + frequency)
     now = datetime.datetime.now()
     sleepTime = round(60 / frequency)
-    while (now < endTime):
+    while (now.timestamp() < endTime.timestamp()):
         print(now)
         print(sleepTime)
         takephotos.capture(now)
         time.sleep(sleepTime)
         now = datetime.datetime.now()
-    
+
 while 1:
     now = datetime.datetime.now()
     for section in conf.sections():
-        begin = datetime.datetime.strptime(conf.get(section, 'begin'), '%Y-%m-%d %H:%M')
-        end = datetime.datetime.strptime(conf.get(section, 'end'), '%Y-%m-%d %H:%M')
+        begin = datetime.datetime.strptime(conf.get(section, 'begin'), '%Y-%m-%d %H:%M %z')
+        end = datetime.datetime.strptime(conf.get(section, 'end'), '%Y-%m-%d %H:%M %z')
         print(onlineThreads)
-        if (now >= begin and now < end and (not section in onlineThreads)):
+        if (now.timestamp() >= begin.timestamp() and now.timestamp() < end.timestamp() and (not section in onlineThreads)):
             PhotosPerMinute = int(conf.get(section, 'PhotosPerMinute'))
             try:
-                print("inside")
+                print(section, "will be started")
                 thread.start_new_thread( PhotoThread, (section, PhotosPerMinute, end, ) )
                 onlineThreads.append(section)
             except:
                 print ("Error: unable to start thread")
+        else:
+            print(section, "not active")
     time.sleep(60)
